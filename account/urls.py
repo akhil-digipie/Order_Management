ccb997afbd8c89aea5b353a1e90daa1d1a5e5f5a
Digipie Tech <akhilp@digipie.net>
2025-07-28 18:05:51 +0530

    Login and Register Model Created

--- account/urls.py
+++ account/urls.py
@@ -4,11 +4,13 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home),
-    path('products/', views.products, name="products"),
+    path('', views.home, name="home"),
+    path('login/', views.loginPage, name="login"),
+    path('register/', views.registerPage, name="register"),
+
     path('customer/<str:id>/',views.customer, name="customer"),
     path('view_customer_list/',views.view_customer_list, name="view_customer_list"),
-    path('create_order/',views.createOrder, name="create_order"),
+    path('create_order/<str:id>/',views.createOrder, name="create_order"),
     path('update_form/<str:id>/', views.updateOrder, name="update_form"),
     path('delete_form/<str:id>/', views.deleteOrder, name="delete_form"),
 ]
--- account/views.py
+++ account/views.py
@@ -1,8 +1,13 @@
+from urllib.request import Request
+
 from django.core.paginator import Paginator
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import *
-from .forms import ModelForm, OrderForm
+from .forms import ModelForm, OrderForm, CreateUserForm
+from django.forms import inlineformset_factory
+from .filters import OrderFilter
+from django.contrib.auth.forms import UserCreationForm
 
 
 def home(request):
@@ -12,7 +17,7 @@ def home(request):
     total_orders_delivered = orders.filter(status = 'Delivered').count()
     total_orders_pending = orders.filter(status = 'Pending').count()
     total_orders = orders.count()
-    context = {'orders': orders, 'customers': customers, 'total_customer' : total_customer, 'total_orders_delivered' : total_orders_delivered, 'total_orders_pending': total_orders_pending , 'total_orders': total_orders}
+    context = {'orders': orders, 'customers': customers, 'total_customer': total_customer, 'total_orders_delivered' : total_orders_delivered, 'total_orders_pending': total_orders_pending , 'total_orders': total_orders}
     return render (request, 'account/dashboard.html', context)
 
 
@@ -23,9 +28,13 @@ def products(request):
 
 def customer(request, id):
     cust = Customer.objects.get(id = id)
-    # customers = Customer.objects.all()
     orders = cust.order_set.all()
-    context = {'cust': cust, 'orders': orders}
+    order_count = orders.count()
+
+    myFilter = OrderFilter(request.GET, queryset=orders)
+    orders = myFilter.qs
+
+    context = {'cust': cust, 'orders': orders, 'order_count' : order_count, 'myFilter' : myFilter}
     return render (request, 'account/customer.html', context)
 
 
@@ -38,15 +47,19 @@ def view_customer_list(request):
 
     return render(request, 'account/view_customers_list.html', {'customers': customers})
 
-def createOrder(request):
-    form = OrderForm()
+def createOrder(request, id):
+    OrderFormSet = inlineformset_factory(Customer, Order, fields=('product', 'status'), extra=5)
+    customer = Customer.objects.get(id = id)
+    formset = OrderFormSet(queryset=Order.objects.none() ,instance=customer)
+    # form = OrderForm(initial={'customer': customer})
     if request.method == 'POST':
         # print('Printing POST: ', request.POST)
-        form = OrderForm(request.POST)
-        if form.is_valid():
-            form.save()
+        # form = OrderForm(request.POST)
+        formset = OrderFormSet(request.POST, instance=customer)
+        if formset.is_valid():
+            formset.save()
             return redirect('/')
-    context = {'form': form}
+    context = {'formset': formset}
     return render(request, 'account/order_form.html', context)
 
 def updateOrder(request, id):
@@ -63,8 +76,31 @@ def updateOrder(request, id):
 
 def deleteOrder(request, id):
     order = Order.objects.get(id=id)
-    form = OrderForm()
-
-    context = {'form': form}
+    if request.method == "POST":
+        order.delete()
+        return redirect('/')
+    context = {'item': order}
     return render(request, 'account/delete_form.html', context)
+
+
+
+
+
+
+
+def registerPage(request):
+    form = CreateUserForm()
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+
+    context = {'form' : form}
+    return render(request, 'account/register.html', context)
+
+
+
+def loginPage(request):
+    context = {}
+    return render(request, 'account/login.html', context)
 
